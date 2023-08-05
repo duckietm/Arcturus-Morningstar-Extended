72a24b10c867e7da38f01182eddc8d06f5dd7544
DuckieTM <info@camwijs.nl>
2023-08-05 13:08:40 +0200

    Update DatabasePool.java

--- src/main/java/com/eu/habbo/database/DatabasePool.java
+++ src/main/java/com/eu/habbo/database/DatabasePool.java
@@ -3,18 +3,30 @@ package com.eu.habbo.database;
 import com.eu.habbo.core.ConfigurationManager;
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 class DatabasePool {
-    private final Logger log = LoggerFactory.getLogger(DatabasePool.class);
+    private static final String DB_POOL_MAX_SIZE = "db.pool.maxsize";
+    private static final String DB_POOL_MIN_SIZE = "db.pool.minsize";
     private HikariDataSource database;
+    private static DatabasePool instance;
+
+    DatabasePool() {
+        // Private constructor for singleton pattern
+    }
+    public static synchronized DatabasePool getInstance() {
+        if (instance == null) {
+            instance = new DatabasePool();
+        }
+        return instance;
+    }
 
     public boolean getStoragePooling(ConfigurationManager config) {
         try {
             HikariConfig databaseConfiguration = new HikariConfig();
-            databaseConfiguration.setMaximumPoolSize(config.getInt("db.pool.maxsize", 50));
-            databaseConfiguration.setMinimumIdle(config.getInt("db.pool.minsize", 10));
+            databaseConfiguration.setMaximumPoolSize(config.getInt(DB_POOL_MAX_SIZE, 50));
+            databaseConfiguration.setMinimumIdle(config.getInt(DB_POOL_MIN_SIZE, 10));
             databaseConfiguration.setJdbcUrl("jdbc:mariadb://" + config.getValue("db.hostname", "localhost") + ":" + config.getValue("db.port", "3306") + "/" + config.getValue("db.database", "habbo") + config.getValue("db.params"));
             databaseConfiguration.addDataSourceProperty("serverName", config.getValue("db.hostname", "localhost"));
             databaseConfiguration.addDataSourceProperty("port", config.getValue("db.port", "3306"));
@@ -40,12 +52,16 @@ class DatabasePool {
             //databaseConfiguration.setDriverClassName("com.mysql.jdbc.jdbc2.optional.MysqlDataSource");
             this.database = new HikariDataSource(databaseConfiguration);
         } catch (Exception e) {
+            log.error("Error initializing database connection pool: {}", e.getMessage());
             return false;
         }
         return true;
     }
 
     public HikariDataSource getDatabase() {
-        return this.database;
+        if (database == null) {
+            throw new IllegalStateException("Database connection pool is not initialized.");
+        }
+        return database;
     }
 }
