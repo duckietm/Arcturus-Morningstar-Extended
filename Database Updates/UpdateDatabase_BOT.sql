b0d3a1f3c1d29e8e943b7d4518cb5be4eea90b6e
duckietm <troop@mailtjes.com>
2024-10-21 13:08:55 +0200

    :up: SQL for new release

---
+++ Database Updates/UpdateDatabase_BOT.sql
@@ -0,0 +1,4 @@
+### New bot walking settings
+
+INSERT INTO `emulator_settings` (`key`, `value`) VALUES ('hotel.bot.limit.walking.distance', '1');
+INSERT INTO `emulator_settings` (`key`, `value`) VALUES ('hotel.bot.limit.walking.distance.radius', '5');
