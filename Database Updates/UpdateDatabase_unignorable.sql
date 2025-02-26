31fd5f2b31970cc4dabf4d5425c19f4158cf2345
duckietm <info@camwijs.nl>
2025-02-26 21:48:02 +0100

    Update UpdateDatabase_unignorable.sql
    Small SQL Fix

--- Database Updates/UpdateDatabase_unignorable.sql
+++ Database Updates/UpdateDatabase_unignorable.sql
@@ -1,2 +1,2 @@
 --New permission
-ALTER TABLE `permissions` ADD COLUMN `acc_unignorable` ENUM('0','1') NOT NULL DEFAULT '0' AFTER `acc_infinite_friends`;
+ALTER TABLE `permissions` ADD COLUMN `acc_unignorable` ENUM('0','1') NOT NULL DEFAULT '0';
