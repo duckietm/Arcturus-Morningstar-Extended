ce103f1855d6535c4c9bea35773d34f46a6e2758
duckietm <troop@mailtjes.com>
2026-04-14 09:54:10 +0200

    🆙 Stage 2 Fix SQL

--- Database Updates/011_add_builders_club_catalog_offers.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE `catalog_club_offers`
-    MODIFY COLUMN `type` ENUM('HC', 'VIP', 'BUILDERS_CLUB', 'BUILDERS_CLUB_ADDON') NOT NULL DEFAULT 'HC';
-
-ALTER TABLE `users_settings`
-    ADD COLUMN `builders_club_bonus_furni` INT NOT NULL DEFAULT 0 AFTER `hc_gifts_claimed`;
